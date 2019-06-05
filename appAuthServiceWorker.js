eba45fbf398a6ddf2ed8feec7096c26e92042c36
Jake Feasel <jake.feasel@forgerock.com>
2019-06-05 16:08:30 -0700

    Refactoring appAuthServiceWorker.js for readability

--- appAuthServiceWorker.js
+++ appAuthServiceWorker.js
@@ -1,155 +1,141 @@
-self.addEventListener("install", () => {
-    self.skipWaiting();
-});
+(function () {
 
-self.addEventListener("activate", () => {
-    self.clients.claim();
-});
+    self.addEventListener("install", () => {
+        self.skipWaiting();
+    });
 
-self.addEventListener("message", (event) => {
-    if (event.data.message === "configuration") {
-        self.appAuthConfig = event.data.config;
-        self.failedRequestQueue = self.failedRequestQueue || {};
-        self.messageChannel = event.ports[0];
-        self.messageChannel.postMessage({
-            "message": "configured"
+    self.addEventListener("activate", () => {
+        self.clients.claim();
+    });
+
+    self.addEventListener("message", (event) => {
+        if (event.data.message === "configuration") {
+            self.appAuthConfig = event.data.config;
+            self.failedRequestQueue = self.failedRequestQueue || {};
+            self.messageChannel = event.ports[0];
+            self.messageChannel.postMessage({
+                "message": "configured"
+            });
+        } else if (event.data.message === "tokensRenewed") {
+            self.retryFailedRequests(event.data.resourceServer);
+        }
+    });
+
+    self.waitForRenewedToken = function (resourceServer) {
+        return new Promise((resolve, reject) => {
+            if (!self.failedRequestQueue[resourceServer]) {
+                self.failedRequestQueue[resourceServer] = [];
+            }
+            self.failedRequestQueue[resourceServer].push([resolve, reject]);
         });
-    } else if (event.data.message === "tokensRenewed") {
-        self.retryFailedRequests(event.data.resourceServer);
-    }
-});
+    };
 
-self.waitForRenewedToken = function (resourceServer) {
-    return new Promise((resolve, reject) => {
-        if (!self.failedRequestQueue[resourceServer]) {
-            self.failedRequestQueue[resourceServer] = [];
+    self.retryFailedRequests = function (resourceServer) {
+        if (self.failedRequestQueue && self.failedRequestQueue[resourceServer]) {
+            var p = self.failedRequestQueue[resourceServer].shift();
+            while (p) {
+                p[0]();
+                p = self.failedRequestQueue[resourceServer].shift();
+            }
         }
-        self.failedRequestQueue[resourceServer].push([resolve, reject]);
-    });
-};
+    };
 
-self.retryFailedRequests = function (resourceServer) {
-    if (self.failedRequestQueue && self.failedRequestQueue[resourceServer]) {
-        var p = self.failedRequestQueue[resourceServer].shift();
-        while (p) {
-            p[0]();
-            p = self.failedRequestQueue[resourceServer].shift();
-        }
-    }
-};
-
-self.addAccessTokenToRequest = function (request, resourceServer) {
-    return new Promise((resolve, reject) => {
-        var dbReq = indexedDB.open("appAuth",1);
-        dbReq.onupgradeneeded = () => {
-            dbReq.result.createObjectStore(this.appAuthConfig.clientId);
-        };
-        dbReq.onerror = reject;
-        dbReq.onsuccess = () => {
-            var objectStoreRequest = dbReq.result.transaction([self.appAuthConfig.clientId], "readonly")
-                .objectStore(self.appAuthConfig.clientId).get("tokens");
-
-            objectStoreRequest.onerror = reject;
-
-            objectStoreRequest.onsuccess = () => {
-                var tokens = objectStoreRequest.result,
-                    rsHeaders =  new Headers(request.headers);
-
-                dbReq.result.close();
-
-                if (!tokens[resourceServer]) {
-                    self.waitForRenewedToken(resourceServer).then(() => {
-                        self.addAccessTokenToRequest(request, resourceServer).then(resolve, reject);
-                    }, reject);
-                    self.messageChannel.postMessage({
-                        "message":"renewTokens",
-                        "resourceServer": resourceServer
-                    });
-                } else {
-                    rsHeaders.set("Authorization", `Bearer ${tokens[resourceServer]}`);
-
-                    resolve(new Request(request.url, {
-                        method: request.method,
-                        headers: rsHeaders,
-                        body: request.body,
-                        mode: request.mode,
-                        credentials: request.credentials,
-                        cache: request.cache,
-                        redirect: request.redirect,
-                        referrer: request.referrer,
-                        integrity: request.integrity
-                    }));
-                }
+    self.addAccessTokenToRequest = function (request, resourceServer) {
+        return new Promise((resolve, reject) => {
+            var dbReq = indexedDB.open("appAuth",1);
+            dbReq.onupgradeneeded = () => {
+                dbReq.result.createObjectStore(this.appAuthConfig.clientId);
             };
-        };
-    });
-};
+            dbReq.onerror = reject;
+            dbReq.onsuccess = () => {
+                var objectStoreRequest = dbReq.result.transaction([self.appAuthConfig.clientId], "readonly")
+                    .objectStore(self.appAuthConfig.clientId).get("tokens");
 
-self.addEventListener("fetch", (event) => {
-    if (self.appAuthConfig &&
-        typeof self.appAuthConfig.resourceServers === "object" &&
-        Object.keys(self.appAuthConfig.resourceServers).length) {
+                objectStoreRequest.onerror = reject;
 
-        var resourceServer = Object.keys(self.appAuthConfig.resourceServers)
-            .filter((rs) => event.request.url.indexOf(rs) === 0)[0];
+                objectStoreRequest.onsuccess = () => {
+                    var tokens = objectStoreRequest.result,
+                        rsHeaders =  new Headers(request.headers);
 
-        if (resourceServer) {
-            event.respondWith(new Promise((resolve, reject) => {
-                self.addAccessTokenToRequest(event.request, resourceServer).then((rsRequest) => {
-                    fetch(rsRequest).then((resp) => {
-                        if (!resp.ok) {
-                            // Watch for errors as described by https://tools.ietf.org/html/rfc6750#section-3
-                            var auth_header = resp.headers.get("www-authenticate");
-                            if (auth_header && auth_header.match(/^Bearer /)) {
-                                var auth_details = auth_header
-                                    .replace(/^Bearer /, "")
-                                    .match(/[^,=]+=".*?"/g)
-                                    .reduce(function (result, detail) {
-                                        var pair = detail.split("=");
-                                        result[pair[0]] = pair[1].replace(/"(.*)"/, "$1");
-                                        return result;
-                                    }, {});
+                    dbReq.result.close();
 
-                                if (auth_details["error"] === "invalid_token") {
-                                    /*
-                                    From https://tools.ietf.org/html/rfc6750#section-3.1:
-                                    invalid_token
-                                         The access token provided is expired, revoked, malformed, or
-                                         invalid for other reasons.  The resource SHOULD respond with
-                                         the HTTP 401 (Unauthorized) status code.  The client MAY
-                                         request a new access token and retry the protected resource
-                                         request.
+                    if (!tokens[resourceServer]) {
+                        self.waitForRenewedToken(resourceServer).then(() => {
+                            self.addAccessTokenToRequest(request, resourceServer).then(resolve, reject);
+                        }, reject);
+                        self.messageChannel.postMessage({
+                            "message":"renewTokens",
+                            "resourceServer": resourceServer
+                        });
+                    } else {
+                        rsHeaders.set("Authorization", `Bearer ${tokens[resourceServer]}`);
 
-                                    We are going to follow the RFC's advice here and try to request a new
-                                    access token and retry the request.
-                                    */
-                                    self.waitForRenewedToken(resourceServer).then(() => {
-                                        self.addAccessTokenToRequest(event.request, resourceServer).then((freshRSRequest) => {
-                                            fetch(freshRSRequest).then(
-                                                (resp) => resolve(resp),
-                                                (failure) => reject(failure)
-                                            );
-                                        });
-                                    }, reject);
-                                    self.messageChannel.postMessage({
-                                        "message":"renewTokens",
-                                        "resourceServer": resourceServer
-                                    });
-                                } else {
-                                    resolve(resp);
-                                }
+                        resolve(new Request(request.url, {
+                            method: request.method,
+                            headers: rsHeaders,
+                            body: request.body,
+                            mode: request.mode,
+                            credentials: request.credentials,
+                            cache: request.cache,
+                            redirect: request.redirect,
+                            referrer: request.referrer,
+                            integrity: request.integrity
+                        }));
+                    }
+                };
+            };
+        });
+    };
+
+    self.addEventListener("fetch", (event) => {
+        if (self.appAuthConfig &&
+            typeof self.appAuthConfig.resourceServers === "object" &&
+            Object.keys(self.appAuthConfig.resourceServers).length) {
+
+            var resourceServer = Object.keys(self.appAuthConfig.resourceServers)
+                .filter((rs) => event.request.url.indexOf(rs) === 0)[0];
+
+            if (resourceServer) {
+                event.respondWith(new Promise((resolve, reject) => {
+                    self.addAccessTokenToRequest(event.request, resourceServer)
+                        .then((rsRequest) => fetch(rsRequest))
+                        .then((resp) => {
+                            // Watch for retry-able errors as described by https://tools.ietf.org/html/rfc6750#section-3
+                            if (!resp.ok && getAuthHeaderDetails(resp.headers)["error"] === "invalid_token") {
+                                let promise = self.waitForRenewedToken(resourceServer)
+                                    .then(() => self.addAccessTokenToRequest(event.request, resourceServer))
+                                    .then((freshRSRequest) => fetch(freshRSRequest));
+
+                                self.messageChannel.postMessage({
+                                    "message":"renewTokens",
+                                    "resourceServer": resourceServer
+                                });
+                                return promise;
                             } else {
-                                resolve(resp);
+                                return resp;
                             }
-                        } else {
-                            resolve(resp);
-                        }
-                    }, reject);
-                });
-            }));
+                        })
+                        .then(resolve, reject);
+                }));
+            }
         }
+        return;
+    });
 
+    function getAuthHeaderDetails(headers) {
+        var authHeader = headers.get("www-authenticate");
+
+        if (authHeader && authHeader.match(/^Bearer /)) {
+            return authHeader.replace(/^Bearer /, "")
+                .match(/[^,=]+=".*?"/g)
+                .reduce(function (result, detail) {
+                    var pair = detail.split("=");
+                    result[pair[0]] = pair[1].replace(/"(.*)"/, "$1");
+                    return result;
+                }, {});
+        } else {
+            return {};
+        }
     }
 
-    return;
-});
+}());
